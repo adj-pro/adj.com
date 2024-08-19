1dc26437ffed9b8fffbf5d6b467c90d30b1307a5
osvaldocariege06 <osvaldocariege06@gmail.com>
2024-08-19 11:47:10 +0100

    build: making the build

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,10 +1,8 @@
-import { useRouteError, Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import { Info } from 'lucide-react'
 import Colors from 'tailwindcss/colors'
 
 export  function ErrorPage() {
-
-  const error: unknown = useRouteError()
 
   return (
     <div id="error-page" className="flex justify-center items-center w-full h-screen bg-hero-pattern bg-no-repeat bg-center">
@@ -14,7 +12,7 @@ export  function ErrorPage() {
             <Info size={82} color={Colors.lime[400]} />
           <h1 className="text-2xl font-bold mt-4">Opss!</h1>
           <p className="text-lg text-zinc-300">Página não encontrada, resultado: <span className="text-lg text-red-300">
-            {error.statusText || error.message}
+          
           </span></p>
           
           <Link to={'/'} className="text-zinc-800 mt-4 font-medium active:scale-95 transition-all hover:bg-lime-800 bg-lime-400 p-4 rounded-md flex justify-center items-center w-28">Voltar</Link>
